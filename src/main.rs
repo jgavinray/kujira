9f86cd89363539432d0d2eddcf0f20333f08876a
J. Gavin Ray <git@jgavinray.com>
2022-09-26 14:44:47 +0100

    feat: Reading data from config file
    Signed-off-by: J. Gavin Ray <git@jgavinray.com>

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,23 @@
-fn main() {
-    println!("Hello, world!");
+#[macro_use]
+extern crate serde_derive;
+
+use std::fs;
+use serde_json::Result;
+
+#[derive(Deserialize, Serialize)]
+struct Config {
+    jiraUrl: String,
+    jiraToken: String,
+}
+
+fn main() -> Result<()> {
+    let file_path = "/opt/kujira/config.json";
+    let data = fs::read_to_string(file_path)
+        .expect("Should have been able to read the file");
+
+    let config: Config = serde_json::from_str(&data)?;
+
+    println!("{:#?}", config.jiraUrl);
+
+    Ok(())
 }
